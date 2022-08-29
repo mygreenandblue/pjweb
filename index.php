37a1e3736feacd835b3d1a359dafd022c5cd14d6
duc2407 <88962809+duc2407@users.noreply.github.com>
2022-08-29 23:35:35 +0700

    change list menu

--- index.php
+++ index.php
@@ -73,19 +73,19 @@ require_once("classes/dbConnection.php");
             <div class="container">
                 <div class="menu">
                     <ul id="mainmenu">
-                        <?
+                    <?php
                         // query menu from db
                         $dbConnection = new dbConnection();
                         $conn = $dbConnection->getConnection();
 
-                        $sql = "SELECT name FROM users";
+                        $sql = "SELECT menu_title FROM menu_list";
                         $result = $conn->query($sql);
                         if ($result->num_rows > 0) {
                             $stt = 0;
                             while ($row = $result->fetch_assoc()) {
                                 $stt++;
                         ?>
-                                <li><a>l1 <?= $row["email"] ?></a></li>
+                                <li><a><?= $row["menu_title"] ?></a></li>
                         <?php
                             }
                         }
@@ -123,7 +123,7 @@ require_once("classes/dbConnection.php");
                 <div class="container-fluid text-center">
                     <img src="images/flower_string.png" width="30%">
                 </div>
-                <?
+                <?php
                 for ($i = 1; $i <= 100; $i++) {
                 ?>
                     <div class="col-md-4 col-sm-6">
@@ -159,7 +159,7 @@ require_once("classes/dbConnection.php");
                             </div>
                         </a>
                     </div>
-                <?
+                <?php
                 }
                 ?>
             </div>
